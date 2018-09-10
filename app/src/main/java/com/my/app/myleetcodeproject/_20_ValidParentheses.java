ba9c75673f8887bbaf9576eef39c04c8d68059eb
ouyang <396586833@qq.com>
2018-09-10 16:33:16 +0800

    Valid Parentheses有效的括号 题目提交

--- app/src/main/java/com/my/app/myleetcodeproject/_20_ValidParentheses.java
+++ app/src/main/java/com/my/app/myleetcodeproject/_20_ValidParentheses.java
@@ -37,10 +37,11 @@ import java.util.Stack;
  */
 
 public class _20_ValidParentheses {
-    
+
+    //这道题考的是栈的相关操作
 
     public static void main(String[] args) {
-        System.out.println(isValid("{[]}")+"");
+        System.out.println(isValid("[{}]") + "");
     }
 
     private static boolean isValid(String s) {
@@ -49,16 +50,32 @@ public class _20_ValidParentheses {
             return true;
 
         Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
+        for (char c : s.toCharArray()) {//将输入的字符串进行遍历，如果是左边的符号，栈入栈对应相反的右边符号
             if (c == '(')
                 stack.push(')');
             else if (c == '{')
                 stack.push('}');
             else if (c == '[')
                 stack.push(']');
-            else if (stack.isEmpty() || stack.pop() != c)
+            else if (stack.isEmpty() || stack.pop() != c) {
+                /*
+                * 如果遍历后，三个左符号都没有匹配到，栈为空，说明输入都字符并不符合左符号先出现的规则
+                *
+                * 或者栈pop出最后一位不等于当前的遍历的字符,返回false
+                *
+                * 为什么要判断栈最后一位是否和现在遍历的字符相等呢？因为左符号都没有匹配到，那么输入的字符就肯定是右符号
+                *
+                * 那么这时候我们要寻找合格的结果是上一个遍历字符是现在这个右字符对应的左字符，比如现在是'}',那么上一个
+                *
+                * 遍历字符必须是'{'结果才成立，由于我们判断左字符，入栈右字符，那么这时候栈顶的字符必须和现在遍历的字符
+                *
+                * 相等，才符合规则
+                * */
                 return false;
+            }
         }
+
+        //在经过 stack.pop == c 的判断后，符合规则的栈应该被清空了，如果栈不为空，说明输入不符合规则
         return stack.isEmpty();
     }
 }
