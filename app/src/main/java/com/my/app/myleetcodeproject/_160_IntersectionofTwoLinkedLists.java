981b58c5b9a9bdf48cdab279feb8b9918685523f
ouyang <396586833@qq.com>
2018-10-15 23:46:31 +0800

    增加题目

--- app/src/main/java/com/my/app/myleetcodeproject/_160_IntersectionofTwoLinkedLists.java
+++ app/src/main/java/com/my/app/myleetcodeproject/_160_IntersectionofTwoLinkedLists.java
@@ -1,5 +1,7 @@
 package com.my.app.myleetcodeproject;
 
+import com.my.app.myleetcodeproject.Model.ListNode;
+
 /**
  * @description: 160. Intersection of Two Linked Lists 相交链表
  * @author: ouyangxin
@@ -13,5 +15,27 @@ public class _160_IntersectionofTwoLinkedLists {
 
     }
 
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (isCircle(headA)&&isCircle(headB)){
+
+        }else if (!isCircle(headA)&&isCircle(headB)){
+
+        }else {
+
+        }
+    }
+
+    private static boolean isCircle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
 
 }
