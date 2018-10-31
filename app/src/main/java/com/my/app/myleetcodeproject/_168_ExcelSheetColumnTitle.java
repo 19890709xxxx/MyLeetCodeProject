af0a4c6f3170353ad3e232c4eb3aad8d58dcb01b
ouyang <396586833@qq.com>
2018-10-31 21:21:26 +0800

    168 增加注释

--- app/src/main/java/com/my/app/myleetcodeproject/_168_ExcelSheetColumnTitle.java
+++ app/src/main/java/com/my/app/myleetcodeproject/_168_ExcelSheetColumnTitle.java
@@ -5,14 +5,38 @@ package com.my.app.myleetcodeproject;
  * @author: ouyangxin
  * @date: 2018-10-31 11:31
  * @version: 1.0
+ * 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+ * <p>
+ * 例如，
+ * <p>
+ * 1 -> A
+ * 2 -> B
+ * 3 -> C
+ * ...
+ * 26 -> Z
+ * 27 -> AA
+ * 28 -> AB
+ * ...
+ * 示例 1:
+ * <p>
+ * 输入: 1
+ * 输出: "A"
+ * 示例 2:
+ * <p>
+ * 输入: 28
+ * 输出: "AB"
+ * 示例 3:
+ * <p>
+ * 输入: 701
+ * 输出: "ZY"
  */
 
 public class _168_ExcelSheetColumnTitle {
 
     //这样生成字母表很不酷， 'A' 其实可以推出所有字母，'B'= 'A'+1 , 'Z' = 'A'+25 ,以后遇到char字符要注意有这种思维
-    private static final char[] chars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    private static final char[] chars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+            'L', 'M', 'N',
             'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
 
 
     public static void main(String[] args) {
@@ -33,7 +57,7 @@ public class _168_ExcelSheetColumnTitle {
              * 3. 对26求余，余数加上 'A'，不过注意这里得到的是字母是从后往前append的，所以最后要反转stringBuilder ，下面的String解法就
              *    不需要反转，因为它把求到的char值都放置在最前
              * */
-            stringBuilder.append((char)('A' + (--n) % 26));// 'A' 这里起了一个初始定位的作用，后面得到的char将会转换成字母
+            stringBuilder.append((char) ('A' + (--n) % 26));// 'A' 这里起了一个初始定位的作用，后面得到的char将会转换成字母
             n /= 26;
         }
         return stringBuilder.reverse().toString();
@@ -42,7 +66,7 @@ public class _168_ExcelSheetColumnTitle {
     public static String convertToTitle2(int n) {
         String title = "";
         while (n > 0) {
-            title = String.valueOf((char)('A' + (--n) % 26)) + title;
+            title = String.valueOf((char) ('A' + (--n) % 26)) + title;
             n /= 26;
         }
         return title;
---
+++ app/src/main/java/com/my/app/myleetcodeproject/_169_MajorityElement.java
@@ -0,0 +1,36 @@
+package com.my.app.myleetcodeproject;
+
+/**
+ * @description: 169.Majority Element 求众数
+ * @author: ouyangxin
+ * @date: 2018-10-31 18:16
+ * @version: 1.0
+ * 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ * <p>
+ * 你可以假设数组是非空的，并且给定的数组总是存在众数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [3,2,3]
+ * 输出: 3
+ * 示例 2:
+ * <p>
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ */
+
+public class _169_MajorityElement {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int majorityElement(int[] nums) {
+       int count =0;
+       int number = nums[0];
+
+       for (int i =0;i<nums.length;i++){
+           
+       }
+    }
+}
